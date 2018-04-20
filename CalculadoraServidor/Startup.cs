fa8c8bf7bc64b135d0e97070bbc61be663141321
nmaria <nmaria@evicertia.com>
2018-04-20 10:56:55 +0200

    rem

--- CalculadoraServidor/Startup.cs
+++ CalculadoraServidor/Startup.cs
@@ -27,8 +27,7 @@ namespace CalculadoraServidor
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-     
-                app.UseExceptionHandler("/Calculator/Error");
+            app.UseExceptionHandler("/Calculator/Error");
 
             app.UseStaticFiles();
 
